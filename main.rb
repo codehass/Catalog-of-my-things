d30b0dc133f3a97423a0b15773ec45af0b5b4704
codehass <hassan.elouardy06@gmail.com>
2023-03-22 10:58:34 +0000

    Add methodes to the app class and fix linters

--- main.rb
+++ main.rb
@@ -1,18 +1,20 @@
+require_relative './app'
+
 class Main
   def menu
     @list = {
-      '1' => "List all books",
-      '2' => "List all labels",
-      '3' => "List all music album ",
-      '4' => "List all genres",
-      '5' => "List of games",
-      '6' => "List all authors",
-      '7' => "Add book",
-      '8' => "Add music album ",
-      '9' => "Add a game",
-      '10' => "Exit",
+      '1' => 'List all books',
+      '2' => 'List all labels',
+      '3' => 'List all music album ',
+      '4' => 'List all genres',
+      '5' => 'List of games',
+      '6' => 'List all authors',
+      '7' => 'Add book',
+      '8' => 'Add music album ',
+      '9' => 'Add a game',
+      '10' => 'Exit'
     }
-  
+
     @list.each do |index, string|
       puts "#{index} - #{string}"
     end
@@ -39,6 +41,7 @@ def choice(user_answer, response)
   action = actions[user_answer]
   response.send(action)
 end
+
 def main
   response = App.new
   loop do
