0591060200c11abfe905f61fb7f693c9d9c3f814
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-03-22 23:17:52 +0530

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,24 @@
+
+CREATE TABLE items(
+    id serial PRIMARY KEY,
+    genre_id VARCHAR(100),
+    author_id VARCHAR(100),
+    label_id VARCHAR(100),
+    source_id VARCHAR(100),
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id),
+    FOREIGN KEY (source_id) REFERENCES sources (id),
+)
+
 CREATE TABLE books (
     id serial primary key,
     title VARCHAR(100),
     publisher VARCHAR(100),
-    cover_state VARCHAR(100)
+    cover_state VARCHAR(100),
+    FOREIGN KEY(id) REFERENCES items(id)
 )
 
 CREATE TABLE labels (
@@ -14,5 +30,6 @@ CREATE TABLE labels (
 CREATE TABLE sources(
     id serial PRIMARY KEY,
     name VARCHAR(255),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    
 );
