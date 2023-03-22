2d7a5d75d9873a1ee65e05767533abdb5e9e8e05
atatm <alelignayana@gmail.com>
2023-03-22 12:46:37 +0300

    handle the choices

--- main.rb
+++ main.rb
@@ -20,3 +20,22 @@ class Main
     gets.chomp.to_i
   end
 end
+
+# handel the choices
+def choice(user_answer, response)
+  actions = {
+    1 => :list_books,
+    2 => :list_labels,
+    3 => :list_music_album,
+    4 => :list_genres,
+    5 => :list_games,
+    6 => :list_authors,
+    7 => :add_book,
+    8 => :add_music_album,
+    9 => :add_game,
+    10 => :exit_app
+  }
+
+  action = actions[user_answer]
+  response.send(action)
+end
