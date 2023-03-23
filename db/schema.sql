9b674c2a8c5281fd448c680d0205f718e6074d33
atatm <alelignayana@gmail.com>
2023-03-23 21:46:06 +0300

    create author table

--- db/schema.sql
+++ db/schema.sql
@@ -55,3 +55,11 @@ CREATE TABLE games (
   last_played_at DATE,
   FOREIGN KEY(id) REFERENCES item(id)
 );
+
+-- Create Authors Table
+CREATE TABLE authors (
+  id  INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  PRIMARY KEY(id)
+);
