1628c37f1f87650058065153ef15f4962fb6a568
atatm <alelignayana@gmail.com>
2023-03-23 21:18:26 +0300

    add load authors in the load data class

--- load_data.rb
+++ load_data.rb
@@ -20,13 +20,23 @@ class Loader
   end
 
   def load_games(games)
-    if File.exist?('./data/games.json')
-      JSON.parse(File.read('./data/games.json')).each do |game|
+    if File.exist?('./JSON/games.json')
+      JSON.parse(File.read('./JSON/games.json')).each do |game|
         games << game
       end
     else
       games = []
     end
   end
-  
+
+  def load_authors(authors)
+    if File.exist?('./JSON/authors.json')
+      JSON.parse(File.read('./JSON/authors.json')).each do |author|
+        authors << author
+      end
+    else
+      authors = []
+    end
+  end
+
 end
