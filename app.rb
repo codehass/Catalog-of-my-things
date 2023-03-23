544c5f4bc895f75d8262d3f87e49231063cb3df6
codehass <hassan.elouardy06@gmail.com>
2023-03-23 14:52:10 +0000

    Send data to arrays as hash

--- app.rb
+++ app.rb
@@ -87,10 +87,17 @@ class App
     on_spotify = %w[y yes].include?(on_spotify)
 
     album = MusicAlbum.new(publish_date, on_spotify)
-    @music_album << album
+    # @music_album << album
     genre = add_genres
     genre.add_item(album)
 
+    albumHash = {
+      'on_spotify' => album.instance_variable_get('@on_spotify'),
+      'publish_date'=> album.instance_variable_get('@publish_date'),
+      'name'=> genre.name
+    }
+
+    @music_album << albumHash
     puts "Album of genre '#{genre.name}' and publish date '#{publish_date}' added successfully!"
   end
 
@@ -99,7 +106,9 @@ class App
     print 'Enter the name of the genre: '
     name = gets.chomp
     genre = Genre.new(name)
-    @genres << genre
+    # @genres << genre
+    @genres << {'name' => genre.name}
+    puts @genres[0]['name']
     genre
   end
 
